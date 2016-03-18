d0793b98d25fd244c6ec167e6d8d599853a7f800
Omri Kaplan <omrikap@gmail.com>
2016-03-18 18:45:23 +0200

    implementing time_handler

--- uthreads.cpp
+++ uthreads.cpp
@@ -7,10 +7,56 @@
 #include <iostream>
 #include "uthreads.h"
 
-#define SECOND 1000000
 #define STACK_SIZE 4096
+#define MAIN_THREAD 0
+
+#ifdef __x86_64__
+/* code for 64 bit Intel arch */
+
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+            "rol    $0x11,%0\n"
+    : "=g" (ret)
+    : "0" (addr));
+    return ret;
+}
+
+#else
+/* code for 32 bit Intel arch */
+
+typedef unsigned int address_t;
+#define JB_SP 4
+#define JB_PC 5
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%gs:0x18,%0\n"
+		"rol    $0x9,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+
+#endif
+
+//////////////////////
+/// Uthreads Utils ///
+//////////////////////
 
 namespace uthreads_utils {
+    itimerval timer;
+    sigaction sig_handler;
     enum status_t {Ready, Blocked, Sleeping, Running};
     unsigned int generalQuantaCounter = 1;
     Thread threads[MAX_THREAD_NUM];
@@ -19,9 +65,57 @@ namespace uthreads_utils {
     std::vector<Thread> blockedThreads;
     std::vector<Thread> sleepingThreads; // todo remove if not needed
     Thread* runningThread;
+    int quantumLength;
+
+    // the scheduler mechanics
+    void timer_handler(int sig)
+        {
+            // Block SIGVTALRM
+            sigset_t set;
+            sigemptyset(&set);
+            sigaddset(&set, SIGVTALRM);
+            sigprocmask(SIG_SETMASK, &set, NULL);
+
+            //todo handle signal
+
+            // Round Robin alg.
+            for (std::vector<Thread>::iterator it = sleepingThreads.begin();
+                    it != sleepingThreads.end(); ++it) {
+                // todo if sleeping over, push_back to ready
+            }
+
+            if (runningThread->getStatus() == Blocked) {
+
+            }
+
+
+            // unblock SIGVTALRM
+            sigprocmask(SIG_BLOCK, &set, NULL);
+
+            int ret_val = sigsetjmp(runningThread->env,1);
+
+            // reset the timer
+            timer.it_value.tv_usec = quantumLength;
+
+            if (ret_val == 1) {
+                return;
+            }
+
+            siglongjmp(readyThreads[0].env,1); // todo decide how to
+                                               // implement the queue
+        }
 }
 
+///////////////////////////////
+/// Library Implementations ///
+///////////////////////////////
+
+using namespace uthreads_utils;
+
 int uthread_init(int quantum_usecs) {
+
+    // initialize the quantum length in micro seconds.
+    quantumLength = quantum_usecs;
 
     // check that parameter is positive
     if (quantum_usecs <= 0) {
@@ -29,33 +123,11 @@ int uthread_init(int quantum_usecs) {
         return -1;
     }
 
-    itimerval timer;
-    sigaction sig_handler;
-
-    // the scheduler mechanics
-    void timer_handler(int sig)
-    {
-        // Block SIGVTALRM
-        sigset_t set;
-        sigemptyset(&set);
-        sigaddset(&set, SIGVTALRM);
-        sigprocmask(SIG_SETMASK, &set, NULL);
-
-        //todo handle signal
-
-
-        // unblock SIGVTALRM
-        sigprocmask(SIG_BLOCK, &set, NULL);
-
-        // reset the timer
-        timer.it_value.tv_usec = quantum_usecs;
-    }
-
     // Install timer_handler as the signal handler for SIGVTALRM.
-    sig_handler.sa_handler = &timer_handler;
-    if (sigaction(SIGVTALRM, &sig_handler, NULL) < 0) {
+    sig_handler.sa_handler = timer_handler;
+    if (sigaction(SIGVTALRM, &(sig_handler), NULL) < 0) {
         std::cerr << "system error: sigaction failed with errno: " << errno << std::endl;
-        return -1;
+        exit(1);
     }
 
     // Configure the timer to expire after "quantum_usecs".
@@ -67,13 +139,13 @@ int uthread_init(int quantum_usecs) {
     timer.it_interval.tv_usec = 0;
 
     // Start a virtual timer. It counts down whenever this process is executing.
-    if (setitimer(ITIMER_VIRTUAL, &timer, nullptr)){
+    if (setitimer(ITIMER_VIRTUAL, &(timer), nullptr)){
         std::cerr << "system error: sigaction failed with errno: " << errno << std::endl;
-        return -1;
+        exit(1);
     }
 
-    Thread main_thread(nullptr);
-    uthreads_utils::threads[0] = main_thread;
+    Thread main_thread(nullptr, MAIN_THREAD);
+    threads[0] = main_thread;
     return 0;
 }
 
@@ -85,13 +157,27 @@ class Thread {
     using namespace uthreads_utils;
 
 public:
-    jmp_buf env;
-    Thread(void *entryPoint);
+    sigjmp_buf env;
+    Thread(void *entryPoint, const unsigned int id);
 
 private:
+public:
+    unsigned int getId() const {
+        return id;
+    }
+
+    unsigned int getQuantaCounter() const {
+        return quantaCounter;
+    }
+
+private:
+    unsigned int id;
     status_t status;
     void *entry_point;
-    unsigned int quantaCounter;
+public:
+    void setQuantaCounter(unsigned int quantaCounter) {
+        Thread::quantaCounter = quantaCounter;
+    }
 
     void setStatus (status_t new_status){
         Thread::status = new_status;
@@ -100,12 +186,17 @@ private:
     const status_t getStatus() const {
         return status;
     }
+
+private:
+    unsigned int quantaCounter;
+    char stack[STACK_SIZE];
 };
 
 //////////////////////////////
 /// Thread Implementations ///
 //////////////////////////////
 
-Thread::Thread(void *entryPoint) {
-    entry_point = entryPoint;
+Thread::Thread(void *entryPoint, const unsigned int id) {
+    Thread::id = id;
+    Thread::entry_point = entryPoint;
 }
